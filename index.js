d418d1afc97ca3bd048f30863c927f92ea246fcc
RB <jsriley24505@gmail.com>
2024-01-28 21:01:50 -0600

    added inquirer questions array for logo creation

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What three letters would you like your logo to say?',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'What color would you like your letters to be?',
+        name: 'lettercolor',
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like your logo to be?',
+        choices: ['circle', 'square', 'triangle'],
+        name: 'shape',
+    },
+    {
+        type: 'input',
+        message: 'What color would you like your shape to be?',
+        name: 'shapecolor',
+    },
+]
